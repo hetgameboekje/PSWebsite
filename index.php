e0f35bd948b451f16bdde49f9f863ee4377dd9f1
HetGameBoekje <514527@vistacollege.nl>
2023-03-20 18:05:41 +0100

    update

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
-<?php
-include_once "navbar.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="index.css">
+    <title>Home page</title>
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
+
+<?php
+include_once "navbar.php";
+?>
+<!-- https://coolors.co/f3e8ee-7b9e87-0b132b-1c2541-3a506b -->
 <body>
-    
+    <br>
+    <div class="photoheader">
+        <h2>My best photo</h2>
+        <div class="photo"></div>
+    </div>
+    <div class="photoheader">
+        <h2>My best photo</h2>
+        <div class="photo"></div>
+    </div>
+    <div class="photoheader">
+        <h2>My best photo</h2>
+        <div class="photo"></div>
+    </div>
+
 </body>
+
 </html>
--- navbar.php
+++ navbar.php
@@ -4,17 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>It works!</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     
     <div class="navbar">
         <ul>
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="social.php">Socials</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="about.php">About</a></li>
           </ul>
     </div>
 </iframe>
